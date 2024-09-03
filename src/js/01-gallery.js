e10ba3ebaf03f810d32cf392ac00f4f538081eee
mihaiciubotaru <mihai.cpp@gmail.com>
2024-09-03 14:58:55 +0300

    Ex 2 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,11 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+// // Descris în documentație
+// import SimpleLightbox from "simplelightbox";
+// // Import suplimentar de stil
+// import "simplelightbox/dist/simple-lightbox.min.css";
+// // Add imports above this line
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+// console.log(galleryItems);
+console.log("START");
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
+player.on('play', function() {
+    console.log('played the video!');
+    player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+});
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+
+player.on('timeupdate',()=>{
+    player.getCurrentTime().then((seconds) => {
+        localStorage.setItem('videoplayer-current-time', seconds);
+    })
+    .catch((error) => {
+        // an error occurred
+        console.log(error);
+    });
+})
